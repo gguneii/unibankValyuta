2dc651fb4e5d232e8667087b00276d49b0ad2d6f
gguneii <guneysuleymanofa@gmail.com>
2024-09-16 20:53:22 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,307 @@
+let data = {
+	Valute: [{
+			"Nominal": "1",
+			"Name": "1 ABŞ dolları",
+			"Value": "1.7",
+			"_Code": "USD"
+		},
+		{
+			"Nominal": "1",
+			"Name": "1 Avro",
+			"Value": "1.8843",
+			"_Code": "EUR"
+		},
+		{
+			"Nominal": "1",
+			"Name": "1 Avstraliya dolları",
+			"Value": "1.1431",
+			"_Code": "AUD"
+		},
+		{
+			"Nominal": "1",
+			"Name": "1 Argentina pesosu",
+			"Value": "0.0018",
+			"_Code": "ARS"
+		},
+		{
+			"Nominal": "1",
+			"Name": "1 Belarus rublu",
+			"Value": "0.5195",
+			"_Code": "BYN"
+		},
+		{
+			"Nominal": "1",
+			"Name": "1 Braziliya realı",
+			"Value": "0.3022",
+			"_Code": "BRL"
+		},
+		{
+			"Nominal": "1",
+			"Name": "1 BƏƏ dirhəmi",
+			"Value": "0.4628",
+			"_Code": "AED"
+		},
+		{
+			"Nominal": "1",
+			"Name": "1 Cənubi Afrika randı",
+			"Value": "0.0959",
+			"_Code": "ZAR"
+		},
+		{
+			"Nominal": "100",
+			"Name": "100 Cənubi Koreya vonu",
+			"Value": "0.1279",
+			"_Code": "KRW"
+		},
+		{
+			"Nominal": "1",
+			"Name": "1 Çexiya kronu",
+			"Value": "0.075",
+			"_Code": "CZK"
+		},
+		{
+			"Nominal": "100",
+			"Name": "100 Çili pesosu",
+			"Value": "0.1826",
+			"_Code": "CLP"
+		},
+		{
+			"Nominal": "1",
+			"Name": "1 Çin yuanı",
+			"Value": "0.2392",
+			"_Code": "CNY"
+		},
+		{
+			"Nominal": "1",
+			"Name": "1 Danimarka kronu",
+			"Value": "0.2525",
+			"_Code": "DKK"
+		},
+		{
+			"Nominal": "1",
+			"Name": "1 Gürcüstan larisi",
+			"Value": "0.6304",
+			"_Code": "GEL"
+		},
+		{
+			"Nominal": "1",
+			"Name": "1 Honq Konq dolları",
+			"Value": "0.218",
+			"_Code": "HKD"
+		},
+		{
+			"Nominal": "1",
+			"Name": "1 Hindistan rupisi",
+			"Value": "0.0203",
+			"_Code": "INR"
+		},
+		{
+			"Nominal": "1",
+			"Name": "1 İngiltərə funt sterlinqi",
+			"Value": "2.2351",
+			"_Code": "GBP"
+		},
+		{
+			"Nominal": "100",
+			"Name": "100 İndoneziya rupiası",
+			"Value": "0.011",
+			"_Code": "IDR"
+		},
+		{
+			"Nominal": "100",
+			"Name": "100 İran rialı",
+			"Value": "0.004",
+			"_Code": "IRR"
+		},
+		{
+			"Nominal": "1",
+			"Name": "1 İsveç kronu",
+			"Value": "0.1656",
+			"_Code": "SEK"
+		},
+		{
+			"Nominal": "1",
+			"Name": "1 İsveçrə frankı",
+			"Value": "2.0006",
+			"_Code": "CHF"
+		},
+		{
+			"Nominal": "1",
+			"Name": "1 İsrail şekeli",
+			"Value": "0.4581",
+			"_Code": "ILS"
+		},
+		{
+			"Nominal": "1",
+			"Name": "1 Kanada dolları",
+			"Value": "1.2522",
+			"_Code": "CAD"
+		},
+		{
+			"Nominal": "1",
+			"Name": "1 Küveyt dinarı",
+			"Value": "5.5692",
+			"_Code": "KWD"
+		},
+		{
+			"Nominal": "1",
+			"Name": "1 Qazaxıstan tengəsi",
+			"Value": "0.0035",
+			"_Code": "KZT"
+		},
+		{
+			"Nominal": "1",
+			"Name": "1 Qırğızıstan somu",
+			"Value": "0.0201",
+			"_Code": "KGS"
+		},
+		{
+			"Nominal": "100",
+			"Name": "100 Livan funtu",
+			"Value": "0.0019",
+			"_Code": "LBP"
+		},
+		{
+			"Nominal": "1",
+			"Name": "1 Malayziya rinqqiti",
+			"Value": "0.394",
+			"_Code": "MYR"
+		},
+		{
+			"Nominal": "1",
+			"Name": "1 Meksika pesosu",
+			"Value": "0.0873",
+			"_Code": "MXN"
+		},
+		{
+			"Nominal": "1",
+			"Name": "1 Moldova leyi",
+			"Value": "0.0977",
+			"_Code": "MDL"
+		},
+		{
+			"Nominal": "1",
+			"Name": "1 Misir funtu",
+			"Value": "0.0351",
+			"_Code": "EGP"
+		},
+		{
+			"Nominal": "1",
+			"Name": "1 Norveç kronu",
+			"Value": "0.1588",
+			"_Code": "NOK"
+		},
+		{
+			"Nominal": "100",
+			"Name": "100 Özbəkistan somu",
+			"Value": "0.0134",
+			"_Code": "UZS"
+		},
+		{
+			"Nominal": "1",
+			"Name": "1 Polşa zlotısı",
+			"Value": "0.4397",
+			"_Code": "PLN"
+		},
+		{
+			"Nominal": "1",
+			"Name": "1 Rusiya rublu",
+			"Value": "0.0191",
+			"_Code": "RUB"
+		},
+		{
+			"Nominal": "1",
+			"Name": "1 Sinqapur dolları",
+			"Value": "1.3084",
+			"_Code": "SGD"
+		},
+		{
+			"Nominal": "1",
+			"Name": "1 Səudiyyə Ərəbistanı rialı",
+			"Value": "0.453",
+			"_Code": "SAR"
+		},
+		{
+			"Nominal": "1",
+			"Name": "1 SDR (BVF-nin xüsusi borcalma hüquqları)",
+			"Value": "2.2852",
+			"_Code": "SDR"
+		},
+		{
+			"Nominal": "1",
+			"Name": "1 Türkiyə lirəsi",
+			"Value": "0.05",
+			"_Code": "TRY"
+		},
+		{
+			"Nominal": "1",
+			"Name": "1 Tayvan dolları",
+			"Value": "0.0531",
+			"_Code": "TWD"
+		},
+		{
+			"Nominal": "1",
+			"Name": "1 Tacikistan somonisi",
+			"Value": "0.1599",
+			"_Code": "TJS"
+		},
+		{
+			"Nominal": "1",
+			"Name": "1 Türkmənistan manatı",
+			"Value": "0.4857",
+			"_Code": "TMT"
+		},
+		{
+			"Nominal": "1",
+			"Name": "1 Ukrayna qrivnası",
+			"Value": "0.0412",
+			"_Code": "UAH"
+		},
+		{
+			"Nominal": "100",
+			"Name": "100 Yaponiya yeni",
+			"Value": "1.2052",
+			"_Code": "JPY"
+		},
+		{
+			"Nominal": "1",
+			"Name": "1 Yeni Zelandiya dolları",
+			"Value": "1.0512",
+			"_Code": "NZD"
+		}
+	],
+}
+
+
+function toggleDropdown(event) {
+	const dropdown = event.currentTarget.nextElementSibling;
+	dropdown.style.display = dropdown.style.display === 'block' ? 'none' : 'block';
+}
+let fromCurrency = 'USD';
+let toCurrency = 'AZN';
+
+function selectOption(currency, flag, event) {
+	const selectedOption = event.currentTarget.closest('.custom-select').querySelector('.selected-option');
+	selectedOption.innerHTML = `<img src="${flag}" alt="flag"> ${currency}`;
+
+	const dropdown = event.currentTarget.closest('.dropdown');
+	dropdown.style.display = 'none';
+
+	if (selectedOption.closest('.down')) {
+		toCurrency = currency;
+	} else {
+		fromCurrency = currency;
+	}
+	calc()
+}
+
+function calc() {
+	let inputFrom = document.getElementById('inputFrom').value;
+	let fromValute = data.Valute.find(item => item._Code === fromCurrency);
+	let toValute = data.Valute.find(item => item._Code === toCurrency);
+	let result = (inputFrom * fromValute.Value)
+	document.getElementById('inputTo').value = result.toFixed(4);
+	console.log(result);
+	
+}
+calc()
